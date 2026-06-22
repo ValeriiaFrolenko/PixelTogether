c28f2c80ed91e10a7177b68321af18accdc9b043
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-06-23 01:09:10 +0300

    feat: refactor PixelTogetherApp to use ViewManager for scene initialization and navigation

--- Client/src/main/java/frolenko/client/PixelTogetherApp.java
+++ Client/src/main/java/frolenko/client/PixelTogetherApp.java
@@ -2,9 +2,12 @@ package frolenko.client;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import frolenko.client.config.AppView;
 import frolenko.client.core.ClientModule;
+import frolenko.client.core.ViewManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import frolenko.client.network.ClientConnection;
@@ -22,9 +25,11 @@ public class PixelTogetherApp extends Application {
         connectionThread.setDaemon(true);
         connectionThread.start();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(PixelTogetherApp.class.getResource("view/main-view.fxml"));
-        fxmlLoader.setControllerFactory(injector::getInstance);
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        ViewManager viewManager = injector.getInstance(ViewManager.class);
+        Parent root = viewManager.initialize();
+        viewManager.navigateTo(AppView.MAIN);
+
+        Scene scene = new Scene(root, 800, 600);
         stage.setTitle("PixelTogether");
         stage.setScene(scene);
         stage.show();
